7fe8e2e3f5e24ae7ee1d71136a5d15e91630eaab
Nikita Soroka <nikitasoroka228@gmail.com>
2022-09-22 11:28:29 +0300

    LR2_T0 is done

---
+++ LR2_Task0/LR2_Task0.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+//REL
+
+void REL(string buf)
+{
+    ofstream fout("output.txt");
+    char letter = buf[0];
+    int count = 1;
+    for (int i = 1; i < buf.length();i++)
+    {
+        if (letter == buf[i])
+        {
+            count++;
+        }
+        else
+            if (count == 1)
+            {
+                fout << letter;
+                letter = buf[i];
+                count = 1;
+            }
+            else
+            {
+                fout << count << letter;
+                letter = buf[i];
+                count = 1;
+            }
+    }
+    if (count == 1)
+        fout << letter;
+    else
+        fout << count << letter;
+
+}
+
+int main()
+{
+    ifstream fin("input.txt");
+    if (!fin.is_open())
+    {
+        cout << "Невозможно открыть файл\n";
+    }
+    else
+    {
+        string buffer;
+        fin >> buffer;
+        REL(buffer);
+    }
+}
